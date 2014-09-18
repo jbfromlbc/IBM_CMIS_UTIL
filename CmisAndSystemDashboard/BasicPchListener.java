63d71ae2f1a90ba94d1c665aa00bfcad81d47f57
Jay Brown <jay.a.brown@gmail.com>
2014-09-18 10:14:28 -0700

    Add thread safety to init code

--- CmisAndSystemDashboard/BasicPchListener.java
+++ CmisAndSystemDashboard/BasicPchListener.java
@@ -38,6 +38,7 @@
 package com.ibm.ecm.cmis.extensions;
 
 import java.math.BigInteger;
+
 import org.apache.chemistry.opencmis.commons.data.ContentStream;
 import org.apache.chemistry.opencmis.commons.data.ExtensionsData;
 import org.apache.chemistry.opencmis.commons.data.ObjectInFolderList;
@@ -107,54 +108,55 @@ public class BasicPchListener extends AbstractCmisServiceWrapper {
 		// Depending on your implementation you may only see this called once.
 
 		// initialize the PCH listener stuff
-		if (listener == null) {
-			listener = new Listener("CMIS V 1.1 PCH server extension", "0.1",
-					null, true);
-
-			// setup all of the other objects now as well
-			cmisContainer = listener.lookupContainer("cmisContainer");
-
-			// this one will be a catch all for any requests
-			// (unfiltered by type)
-			events[ALL_REQUESTS_INDEX] = cmisContainer.lookupEvent(
-					PCHeventClass.RPC, "allEvents");
-
-			/**
-			 * Example of how to manually set up the events :
-			 *  
-			 * // filtered events events[GET_CHILDREN_EVENT_INDEX] =
-			 * cmisContainer.lookupEvent( PCHeventClass.RPC, "getChildren");
-			 * events[QUERY_EVENT_INDEX] = cmisContainer.lookupEvent(
-			 * PCHeventClass.RPC, "query"); events[GET_CONTENT_STREAM_INDEX] =
-			 * cmisContainer.lookupEvent( PCHeventClass.RPC,
-			 * "getContentStream");
-			 * 
-			 * // create array for events and durations // one for each CMIS
-			 * operation durations[GET_CHILDREN_EVENT_INDEX] =
-			 * events[GET_CHILDREN_EVENT_INDEX]
-			 * .lookupAccumulator("getChildrenTime");
-			 * durations[QUERY_EVENT_INDEX] = events[QUERY_EVENT_INDEX]
-			 * .lookupAccumulator("queryTime");
-			 * durations[GET_CONTENT_STREAM_INDEX] =
-			 * events[GET_CONTENT_STREAM_INDEX]
-			 * .lookupAccumulator("getContentStreamTime");
-			 * 
-			 * Loop below handles any that you put in the events list automatically
-			 */
-
-			// Setup all the events that are defined in the eventNames array.			
-			// Starting at 1 since we want to skip the allEvents event.
-			for (int i = 1; i < events.length; i++) {
-
-				String tempEvent = eventNames[i];
-				System.out.println("setting up event:" + tempEvent);
-				// setup all the event objects
-				events[i] = cmisContainer.lookupEvent(PCHeventClass.RPC, tempEvent);
-				// and their matching accumulator named xxxTime.
-				durations[i] = events[i].lookupAccumulator(tempEvent + "Time");
+		// add a sync block since this could be re-entered while processing first time
+		synchronized(events) {
+			if (listener == null) {
+				listener = new Listener("CMIS V 1.1 PCH server extension", "0.1",
+						null, true);
+	
+				// setup all of the other objects now as well
+				cmisContainer = listener.lookupContainer("cmisContainer");
+	
+				// this one will be a catch all for any requests
+				// (unfiltered by type)
+				events[ALL_REQUESTS_INDEX] = cmisContainer.lookupEvent(
+						PCHeventClass.RPC, "allEvents");
+	
+				/**
+				 * Example of how to manually set up the events :
+				 *  
+				 * // filtered events events[GET_CHILDREN_EVENT_INDEX] =
+				 * cmisContainer.lookupEvent( PCHeventClass.RPC, "getChildren");
+				 * events[QUERY_EVENT_INDEX] = cmisContainer.lookupEvent(
+				 * PCHeventClass.RPC, "query"); events[GET_CONTENT_STREAM_INDEX] =
+				 * cmisContainer.lookupEvent( PCHeventClass.RPC,
+				 * "getContentStream");
+				 * 
+				 * // create array for events and durations // one for each CMIS
+				 * operation durations[GET_CHILDREN_EVENT_INDEX] =
+				 * events[GET_CHILDREN_EVENT_INDEX]
+				 * .lookupAccumulator("getChildrenTime");
+				 * durations[QUERY_EVENT_INDEX] = events[QUERY_EVENT_INDEX]
+				 * .lookupAccumulator("queryTime");
+				 * durations[GET_CONTENT_STREAM_INDEX] =
+				 * events[GET_CONTENT_STREAM_INDEX]
+				 * .lookupAccumulator("getContentStreamTime");
+				 * 
+				 * Loop below handles any that you put in the events list automatically
+				 */
+	
+				// starting at 1 since we want to skip the allEvents event.
+				for (int i = 1; i < events.length; i++) {
+	
+					String tempEvent = eventNames[i];
+					System.out.println("setting up event:" + tempEvent);
+					// setup all the event objects
+					events[i] = cmisContainer.lookupEvent(PCHeventClass.RPC, tempEvent);
+					// and their matching accumulator named xxxTime.
+					durations[i] = events[i].lookupAccumulator(tempEvent + "Time");
+				}
 			}
-		}
-
+		} // sync block
 		// Record this event - whatever type it might be
 		// All requests will come through the init method. 
 		events[ALL_REQUESTS_INDEX].recordEvent();
